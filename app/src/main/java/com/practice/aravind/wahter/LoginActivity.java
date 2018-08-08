1df66aa6d62a7b84fb329eeb3f6006d99ac7bb8e
aravind <cool_carefree@zoho.com>
2018-08-08 23:59:43 +0530

    added all string for activities in strings.xml. changes file names - index and login

--- app/src/main/java/com/practice/aravind/wahter/LoginActivity.java
+++ app/src/main/java/com/practice/aravind/wahter/LoginActivity.java
@@ -1,41 +1,16 @@
 package com.practice.aravind.wahter;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
 
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends AppCompatActivity {
-
-
-
+public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
     }
 
-    public void onRegister(View v) {
-        // Switching to Register screen
-        Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-        startActivity(i);
-    }
-
-    public void onLogin(View v) {
-        // Switching to Login screen
-        Intent i = new Intent(getApplicationContext(), PwdActivity.class);
-        startActivity(i);
-    }
+    //todo validation for login -- phone number and password
 
 }
-
-
 
