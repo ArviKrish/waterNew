21cb42c17e3a251d41b3a6c0bd49adc52be3ab0b
ak <akharry100@gmail.com>
2018-09-19 23:58:08 +0530

    conditionn screen  and button resize

---
+++ app/src/main/java/com/practice/aravind/wahter/util/ConditionActivity.java
@@ -0,0 +1,41 @@
+package com.practice.aravind.wahter.util;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.practice.aravind.wahter.DeliveryInfoActivity;
+import com.practice.aravind.wahter.R;
+import com.practice.aravind.wahter.UserInfoActivity;
+
+public class ConditionActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_condition);
+            final Bundle signUpBundle = getIntent().getExtras();
+
+       /* companyName = signUpBundle.getString("companyName");
+        companyType = signUpBundle.getString("companyType");
+        companyOption = signUpBundle.getString("companyOption");
+        companyEmailAddress = signUpBundle.getString("companyEmailAddress");
+        mobileNumber = signUpBundle.getString("mobileNumber");
+        userName = signUpBundle.getString("userName");
+        userDesignation = signUpBundle.getString("userDesignation");
+        userPhoneNumber = signUpBundle.getString("userPhoneNumber");
+        userAltNumber = signUpBundle.getString("userAltNumber");
+        userEmail = signUpBundle.getString("userEmail");
+*/
+
+
+
+
+
+        }
+    }
